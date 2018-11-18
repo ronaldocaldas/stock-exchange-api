b8ca434b9eb7f7eb603050fa5a4f056210fe0efb
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2018-11-18 11:13:38 -0200

    Adicionando banco de dados na aplicação

---
+++ src/main/java/br/com/stock/exchange/api/StockExchangeApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.stock.exchange.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockExchangeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockExchangeApiApplication.class, args);
+	}
+}
