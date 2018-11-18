6c1449ab8371484d33ba85347da5fb8d5b509dd4
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2018-11-18 12:34:43 -0200

    Implementação do post e busca por código de categorias.

--- src/main/java/br/com/stock/exchange/api/resource/CategoriaResource.java
+++ src/main/java/br/com/stock/exchange/api/resource/CategoriaResource.java
@@ -2,8 +2,15 @@ package br.com.stock.exchange.api.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +23,20 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@GetMapping
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
+	}
+
+	@GetMapping("{codigo}")
+	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+		return categoriaRepository.findOne(codigo);
 	}
 }
